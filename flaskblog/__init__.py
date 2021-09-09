62d2e6024cc598786504eda3e2eb6f7fdadc6a7b
Tanish Gupta <guptatanish905@gmail.com>
2021-09-09 15:46:24 +0530

    initial commit

---
+++ flaskblog/__init__.py
@@ -0,0 +1,41 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+
+app = Flask(__name__)
+
+# setting config
+app.config["SECRET_KEY"] = "5791628bb0b13ce0c676dfde280ba245"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:admin@localhost/postdb"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["MAIL_SERVER"] = "smtp.googlemail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = os.environ.get("EMAIL_USER")
+app.config["MAIL_PASSWORD"] = os.environ.get("EMAIL_PASS")
+
+# Creating Instances
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+mail = Mail(app)
+db.init_app(app)
+bcrypt.init_app(app)
+
+# initilize
+login_manager.init_app(app)
+mail.init_app(app)
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
+
+# register Blueprint
+from flaskblog.users.routes import users
+from flaskblog.posts.routes import posts
+from flaskblog.main.routes import main
+
+app.register_blueprint(users)
+app.register_blueprint(posts)
+app.register_blueprint(main)
